fd0f8dbe26a5a59d0f5f2afd927345ab26432e35
xiang <xiangshengjun@cloud-link.ltd>
2025-01-17 12:35:19 +0800

    init    初始化项目结构

---
+++ frameworks/base/src/main/java/org/duku/mall/frameworks/starter/base/ApplicationContextHolder.java
@@ -0,0 +1,13 @@
+package org.duku.mall.frameworks.starter.base;
+
+public class ApplicationContextHolder implements ApplicationContextAware {
+
+
+    private  static  ApplicationContext  CONTEXT;
+
+
+
+
+
+
+}
